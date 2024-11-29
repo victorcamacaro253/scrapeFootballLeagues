2b2b8c2c0706eb7c00504b21c520bb3dba004708
Victor C <victorcamacaro253@gmail.com>
2024-11-28 22:31:30 -0400

    Add files via upload

---
+++ public/js/champions.js
@@ -0,0 +1,30 @@
+fetch('/champions')
+.then(response => response.json())
+.then(data => {
+
+    
+    
+ const tbody = document.getElementById('data-body-champions');
+
+ //Usar map para crear  las filas
+ data.equipos.map((equipo,index)=>{
+ const row= document.createElement('tr')
+
+ row.innerHTML = `
+ <td>${data.posiciones[index]}</td>
+ <td> <img  src=${data.img[index]} class="sdc-site-table__cell-image"></td>
+     <td>${equipo}</td>
+     <td>${data.PJ[index]}</td>
+     <td>${data.victorias[index]}</td>
+     <td>${data.empates[index]}</td>
+     <td>${data.derrotas[index]}</td>
+     <td>${data.GA[index]}</td>
+     <td>${data.GC[index]}</td>
+     <td>${data.DF[index]}</td>
+     <td>${data.puntos[index]}</td>
+     `;
+     tbody.appendChild(row);
+
+ })
+})
+.catch(error=> console.error('Error',error))
